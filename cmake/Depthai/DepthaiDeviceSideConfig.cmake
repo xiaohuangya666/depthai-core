79bdb75ca09ac9a975462613809fbfe5b736ed0c
alex-luxonis <alex@luxonis.com>
2020-09-29 05:26:28 +0300

    Add device support for RGB sensor IMX477 (RPi HQ cam)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "986dd413a954497ab76af4f9afdc5b2c8d404423")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e3d2d9d243c591f522acdb0b56afb3e83298007d")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
