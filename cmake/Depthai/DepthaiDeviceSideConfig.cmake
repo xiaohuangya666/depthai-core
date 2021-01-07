727404338746e0a3e5c90166112d200ac1ab54e1
alex-luxonis <alex@luxonis.com>
2021-01-07 09:20:34 +0200

    Update FW: change camera to IMX283, untested

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2a657300391ced9b459f94e7e4dfb0c3f1ef194e")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a4a1d02f64aeea68b53e7f88aff14c5b9c73f7cf")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
