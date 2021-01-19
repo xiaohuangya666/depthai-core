ad886b52e3cabc07a44b83881e874bdfa0bc9e03
alex-luxonis <alex@luxonis.com>
2021-01-19 12:57:07 +0200

    Update FW: OV9282 enable STROBE output

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3e052d34a8e15d96aab8e84e9ba93d1627f61431")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7aef18063bdb0ae01be839b0952a3fb513eb2339")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
