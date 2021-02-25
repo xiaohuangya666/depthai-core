8a7929e386bb9ea5f6621105ea3c451189f8338f
alex-luxonis <alex@luxonis.com>
2021-02-25 18:18:56 +0200

    Update FW: workaround for IMX477 set exposure fail

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7bbcf26be93b648e94257ae196fb93b9c5f4d6e1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1fbc4340d8a6481bf53f0781c13c9f9755e39b2e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
