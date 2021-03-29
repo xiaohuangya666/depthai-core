738a30247d9d7139278179c86c3ead060d423ce6
alex-luxonis <alex@luxonis.com>
2021-03-29 21:58:23 +0300

    Update FW: external cameras init fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "563f668041c5dd1d462a15ce1174a368ded1fbb8")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "108000a726211d08adc749d66ba17dceb7348c79")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
