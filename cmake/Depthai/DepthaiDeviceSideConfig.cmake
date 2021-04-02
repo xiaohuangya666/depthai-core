724a9b9f6216b66ddd55bc04c4a2ce1ec39c30f3
alex-luxonis <alex@luxonis.com>
2021-04-02 18:51:47 +0300

    Update FW: log errors with cameras not detected, fix OV9282 enumeration with 0x20 address / SID high

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "108000a726211d08adc749d66ba17dceb7348c79")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "aaafe58e23e2c76e8c1cc2f9581a57dd1a28ad81")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
