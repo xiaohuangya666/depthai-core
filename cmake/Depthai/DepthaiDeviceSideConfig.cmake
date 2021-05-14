0c959563b5eaacd15c1f6152babbee9c25e0399d
alex-luxonis <alex@luxonis.com>
2021-05-14 04:24:56 +0300

    StereoDepth: add mesh calibration support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c2bf875327990b95a84aaa0e5d5fe05fa68e0592")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "db2b766dd4791763bb274bc3d92693ec3cea01e1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
