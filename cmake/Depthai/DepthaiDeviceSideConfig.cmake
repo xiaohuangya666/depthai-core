8c7029ac8c258d901f05e78b26bfabf0c2904514
alex-luxonis <alex@luxonis.com>
2021-05-15 00:27:02 +0300

    Update FW: fix OV9782 color order

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "db2b766dd4791763bb274bc3d92693ec3cea01e1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2a5c067d9397775b96eac268e2703b730c03c980")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
