e11818198fb41795bb7558f8d6650c8e0e21ecea
alex-luxonis <alex@luxonis.com>
2021-05-20 04:06:36 +0300

    Update FW: fix OV9282 1-lane pixel flipping artifact

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2a5c067d9397775b96eac268e2703b730c03c980")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0d9f0eff712afba600cbd50ee418c8a9617c0121")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
