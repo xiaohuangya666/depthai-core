e828321641bcb3a97ab42ea5c3fa8765eb9a3485
alex-luxonis <alex@luxonis.com>
2021-05-27 00:29:42 +0300

    Update FW: MonoCamera `out` frame type changed: RAW8 -> GRAY8, handle GRAY8 in ImageManip, StereoDepth rectified changed to GRAY8 (regardless of input type: could also be NV12 / YUV420p)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "556a856e85570e0d874d19b305a0080a8f1f58bf")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bf119f3f72fde1a13f101e4747fed8923a00109e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
