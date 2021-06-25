270fb62960082224c267041e23f3b54ae259c6f2
Sachin <sachin.guruswamy@gmail.com>
2021-06-25 13:28:24 -0700

    Updated device side commiy with baseline abs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1b93fa5bce981ec0624688db407231cf8e822b6d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2351a1d41b69aa2135228ca633b3b926b989b681")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
