bc6df8500688c9b4b93790e64c01b74e0f29153a
alex-luxonis <alex@luxonis.com>
2021-06-29 16:26:54 +0300

    Update FW with AR0234 support on RGB port (IMX378/477 disabled for now)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9a5b24699ef3c80e5022aa056510044ce89df059")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d3e0b42c8ba19b7cdbf48a952f6336e42fa7b3c1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
