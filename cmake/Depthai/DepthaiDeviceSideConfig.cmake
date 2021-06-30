d80fe064cf431c4da45e09829594aec45f215842
alex-luxonis <alex@luxonis.com>
2021-06-30 10:10:11 +0300

    Update FW, change VCM driver

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d3e0b42c8ba19b7cdbf48a952f6336e42fa7b3c1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1b683ea905897646702fc3a2c842d3a9c2e70ca6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
