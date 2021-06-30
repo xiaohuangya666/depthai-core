7452a8398b8e7bce66390e1f3c258083125f1283
alex-luxonis <alex@luxonis.com>
2021-06-30 11:46:44 +0300

    Update FW: enabled VCM for AR0234

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1b683ea905897646702fc3a2c842d3a9c2e70ca6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bae4946e1565a9e8fa8a0365f649b97f1d2eccc5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
