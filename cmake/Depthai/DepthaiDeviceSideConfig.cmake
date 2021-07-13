ad4a0e1f40eba284397c37b61c1eb42c3e90034d
alex-luxonis <alex@luxonis.com>
2021-07-13 19:01:35 +0300

    Update FW: CameraControl output `preview` on capture control only

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6c7d5206dca68fde22314bc97c0e437b00930b70")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3035728545cf307451afd0e7bf4b92213a9e5e80")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
