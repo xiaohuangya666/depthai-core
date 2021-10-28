cc2ec896cc0298f8aa53ac2fe99db832f42b6e25
alex-luxonis <alex@luxonis.com>
2021-10-28 03:37:56 +0300

    Update FW: works without audio/I2S external clock

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "10a1203c673dca6f0dfa5643c45dafc2506af3ee")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cf19f8d6766076c8f3c278bfe66ceb2a065d9425")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
