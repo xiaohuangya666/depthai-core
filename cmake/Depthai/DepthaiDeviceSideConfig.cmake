d2b1b51eb6e7ebdd629dd747c00ed50c62cf88c3
alex-luxonis <alex@luxonis.com>
2021-11-27 14:32:38 +0200

    Ethernet not using the PCIe refclk generator IC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "29036665ba3c8416048f602f0bfc82c17d26fc2e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "96e3e99961c904a7f82d92cb33a0213c68b603a8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
